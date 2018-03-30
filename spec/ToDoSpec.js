e924f2a60f8fb0b281b84f8ef854b0caf0e26519
Dave Colburn <davecolburn@mac.com>
2018-03-29 22:04:44 -0400

    First simple tests

--- spec/ToDoSpec.js
+++ spec/ToDoSpec.js
@@ -1,5 +1,10 @@
 describe('Testing the functionality, this is the checklist:', ()=>{
 
+	it('Should know that true is true and false is false', ()=>{
+		expect(1==1).toBe(true);
+		expect(1==0).toBe(false);
+	});
+
 })
 
 
